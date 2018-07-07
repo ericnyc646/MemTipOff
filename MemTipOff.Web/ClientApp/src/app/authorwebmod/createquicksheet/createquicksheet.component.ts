3f516f00971017978f68dda7b5ce8b9ffce0859f
Eric Cestero <eric@ericnyc.com>
2018-07-07 14:08:03 -0400

    create quicksheet:  started working on portion to create quicksheet json

--- MemTipOff.Web/ClientApp/src/app/authorwebmod/createquicksheet/createquicksheet.component.ts
+++ MemTipOff.Web/ClientApp/src/app/authorwebmod/createquicksheet/createquicksheet.component.ts
@@ -10,7 +10,13 @@ import { QuicksheetsioService } from '../../datalayer/quicksheetsio.service';
 export class CreatequicksheetComponent implements OnInit {
   quicksheets: QuickSheetsModel[];
   elementtextarea = ' ';
+  thetopicselected = '';
   theelementselected = 'ch-topic';
+
+  alltopics = [];
+  cheatsheetjson = {
+      topics: []
+  };
 
   constructor(private quicksheetioservice: QuicksheetsioService) { }
 
@@ -22,4 +28,43 @@ export class CreatequicksheetComponent implements OnInit {
     //alert(value);
   }
 
+  onAddClick() {
+    this.elementtextarea = '';
+
+    switch (this.theelementselected) {
+      case "topicdiv":
+          this.addTopic('');
+          break;
+      case "titlesectiondiv":
+          this.addElement('', '');
+          break;
+
+    }
+  }
+
+  addTopic(title: string) {
+    var index = this.alltopics.length + 1;
+    this.alltopics.push(title + '|topicdiv' + index);
+    this.cheatsheetjson.topics.push({ "name": "topicdiv" + index, "displayorder" : 0, "text" : title, elements: [] });
+    this.reloadtopicsdropdown();
+    this.updatejson();
+  }
+
+  addElement(title: string, topic: string) {
+    for (var item in this.cheatsheetjson.topics) {
+      if (this.cheatsheetjson.topics[item]['name'] == topic) {
+          var displayorder = this.cheatsheetjson.topics[item]['elements'].length + 1;
+          var name = topic + "_element" + displayorder;
+          var element = { "name": name, "text": title, "type": this.theelementselected, "displayorder": displayorder };
+          this.cheatsheetjson.topics[item]['elements'].push(element);
+          this.updatejson();
+          return;
+      }
+
+  }
+  }
+
+  reloadtopicsdropdown() {}
+
+  updatejson() {}
 }
